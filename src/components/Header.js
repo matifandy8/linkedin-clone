2b439c52c0c774abf5df65aac96830aecf728025
matifandy <matifandy@gmail.com>
2021-01-13 00:25:04 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notification";
 
 function Header() {
   return (
@@ -15,7 +21,17 @@ function Header() {
           <input type="text" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption icon={HomeIcon} title="home" />
+        <HeaderOption icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption icon={ChatIcon} title="Messaging" />
+        <HeaderOption icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C4D35AQHSjX0yVzp6Hg/profile-framedphoto-shrink_100_100/0/1609694941139?e=1610571600&v=beta&t=cSB5sUMDZuB1eFMeCmGt2c67dSMWF-NSSybCJS8r-Ls"
+          title="me"
+        />
+      </div>
     </div>
   );
 }
